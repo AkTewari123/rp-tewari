d35a8f481e74521d14fba91e77f2296675963c5d
Messef <85040583+AkTewari123@users.noreply.github.com>
2025-03-06 22:16:28 -0500

    pushing changes

--- app.py
+++ app.py
@@ -1,28 +1,22 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-posts = [
-    {'author': 'Gandalf', 'title': 'my hobbit', 'content': 'Remy Mander', 'date': '3/2/24', 'readers': 11003234},
-    {
-        'author': 'Frodo', 'title': 'The Lord of the Rings', 'content': 'Harry Potter\'s nice wand', 'date': '3/5/24', 'readers': 125
-    }
-]
 
 @app.route('/author')
 def author():
     return render_template("author.html")
 @app.route('/')
 def main():
-    return render_template("index.html", title = "About")
+    return render_template("index.html")
 @app.route('/results')
 def results():
-    return render_template("results.html", title = "About")
+    return render_template("results.html")
 @app.route('/procedure')
 def procedure():
-    return render_template("procedure.html", title = "About")
+    return render_template("procedure.html")
 @app.route('/workscited')
 def workscited():
-    return render_template("workscited.html", title = "About")
+    return render_template("workscited.html")
 
 if __name__ == '__main__':
 
